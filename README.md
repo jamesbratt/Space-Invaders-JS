ce7e81ce3fdba7e9b9bd8e89938682e82dd7fbeb
James Bratt <hello@jamesbratt.co.uk>
2019-11-30 21:25:21 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Super Space Alien Wars
+
+A fun little game where you get to fly a spaceship and shoot aliens.
+
+To get it up and running, simply clone the repo...
+
+### `Right click on index.html and select to open in your favourite web browser`
+
+Easy peasy, no Webpack etc etc... you don't even need to have Node installed!
---
+++ SpaceInvaders.js
@@ -0,0 +1,176 @@
+(function () {
+  var TIME = 1000;
+  var CONTROLS = {
+    STEER_LEFT: 'ArrowLeft',
+    STEER_RIGHT: 'ArrowRight',
+    SHOOT: 'Space',
+  };
+
+  var playButton = document.getElementById("play");
+  var timerDiv = document.getElementById("timer");
+  var canvas = document.getElementById("canvas");
+  var context = canvas.getContext("2d");
+
+  var StartGame = function() {
+    this.setAttribute("style", "display: none");
+    game.start();
+    document.addEventListener('keydown', controlShip);
+  }
+
+  var FinishGame = function() {
+    context.clearRect(0, 0, 1000, 400);
+    game.missilesFired = [];
+    game.aliens = [];
+    game.time = TIME;
+
+    playButton.setAttribute("style", "display: inline");
+    document.removeEventListener('keydown', controlShip);
+  }
+
+  var everyinterval = function(n) {
+    if ((game.frame / n) % 1 == 0) {
+      return true;
+    }
+    return false;
+  }
+
+  var controlShip = function(e) {
+    switch(e.code) {
+      case CONTROLS.STEER_LEFT:
+        game.spaceship.x = game.spaceship.x - 10;
+        game.updateGame(true);
+        break;
+      case CONTROLS.STEER_RIGHT:
+        game.spaceship.x = game.spaceship.x + 10;
+        game.updateGame(true);
+        break;
+      case CONTROLS.SHOOT: {
+        var missile = new Missile(game.spaceship.x, game.spaceship.y).render();
+        game.missilesFired.push(missile);
+        game.updateGame(true);
+        break;
+      }
+    }
+  };
+
+  var SpaceShip = function() {
+    this.x = 480;
+    this.y = 380;
+    this.render = function() {
+      context.fillStyle = "red";
+      context.fillRect(this.x, this.y, 20, 20);
+    };
+    return this;
+  }
+
+  var Missile = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.render = function() {
+      context.fillStyle = "blue";
+      context.fillRect(this.x, this.y, 5, 5);
+      return this;
+    };
+  }
+
+  var Alien = function() {
+    this.x = Math.floor(Math.random() * (980 - 20)) + 20;
+    this.y = 0;
+    this.isHit = false;
+    this.render = function() {
+      context.fillStyle = "green";
+      context.fillRect(this.x, this.y, 20, 20);
+      return this;
+    };
+  }
+
+  var Game = function() {
+    this.time = TIME;
+    this.spaceship = null;
+    this.aliens = [];
+    this.missilesFired = [];
+
+    this.timer = null;
+    this.gameRenderInterval = null;
+    this.frame = 0;
+
+    this.start = function() {
+
+      // Launch the spaceship
+      this.spaceship = new SpaceShip();
+      this.spaceship.render();
+
+      // Start the game timer
+      var timerTick = this.tick.bind(this);
+      this.timer = setInterval(function() {
+        timerTick();
+      }, 1000);
+
+      // Start the game renderer
+      var gameRenderer = this.updateGame.bind(this);
+      this.gameRenderInterval = setInterval(function() {
+        gameRenderer();
+      }, 20);
+    };
+
+    this.updateGame = function(isShipMoving = false) {
+      this.frame += 1;
+
+      context.clearRect(0, 0, 1000, 400);
+
+      if (this.frame === 1 || everyinterval(150)) {
+        var alien = new Alien().render();
+        this.aliens.push(alien);
+      }
+
+      this.missilesFired.forEach((missile) => {
+        var alienHit = this.aliens.find(function(alien) {
+          var yOffset = alien.y + 20;
+          var xOffset = alien.x + 20;
+          return !alien.isHit && (missile.y > alien.y && missile.y < yOffset) && (missile.x > alien.x && missile.x < xOffset);
+        });
+
+        if (alienHit) {
+          alienHit.isHit = true;
+        }
+
+        missile.y = missile.y - 1;
+        missile.render();
+      });
+
+      this.aliens.forEach((alien) => {
+        alien.y = alien.y + 1;
+        alien.render();
+      });
+
+      this.aliens = this.aliens.filter(function(alien) {
+        return !alien.isHit
+      });
+
+      this.aliens.forEach(function(alien) {
+        if (!isShipMoving) {
+          alien.y = alien.y + 1;
+        }
+        alien.render();
+      });
+
+      this.spaceship.render();
+    }
+
+    this.tick = function() {
+      this.time -= 1;
+
+      if (this.time === 0) {
+        clearInterval(this.timer);
+        clearInterval(this.gameRenderInterval);
+        FinishGame();
+      }
+
+      timerDiv.innerHTML = this.time;
+    };
+  }
+
+  playButton.addEventListener('click', StartGame);
+
+  var game = new Game();
+})();
